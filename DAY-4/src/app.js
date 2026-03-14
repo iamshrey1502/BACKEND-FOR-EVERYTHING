54d1feef8d58796800e6e8e76cd515d4d7b2baf3
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-14 21:56:02 +0530

    Status codes

--- DAY-4/src/app.js
+++ DAY-4/src/app.js
@@ -18,12 +18,18 @@ app.post("/notes",(req,res)=>{
 
     notes.push(req.body);
     console.log(notes);
-    res.send("note created");
+    // res.send("note created");
+    res.status(201).json({
+        message:"note created successfuly"
+    });
 
 });
 
 app.get("/notes",(req,res)=>{
-    res.send(notes);
+    // res.send(notes);
+    res.status(200).json({
+        notes:notes
+    });
 
 });
 
@@ -32,7 +38,11 @@ app.get("/notes",(req,res)=>{
 app.delete("/notes/:index",(req,res)=>{
     // console.log(req.params.index);
     delete notes[req.params.index];
-    res.send(notes);
+    // res.send(notes);
+    res.status(204).json({
+        message:"note deleted successfuly"
+
+    });
 
 });
 
@@ -40,7 +50,14 @@ app.delete("/notes/:index",(req,res)=>{
 //PATCH-> partial replacement of data
 app.patch("/notes/:index",(req,res)=>{
     notes[req.params.index].description=req.body.description;
-    res.send("note updated sucessfully");
+    // res.send("note updated sucessfully");      
 });
 
 module.exports=app;
+
+//Status codes->
+//200->successful GET request
+//201->successful POST request
+//204->successful DELETE request
+
+//cluster-> combination of storage + processor
