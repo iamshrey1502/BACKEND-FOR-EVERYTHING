20078b8f49e1cd948729457d8467c1354ab0281d
Shrey Mishra <shreymishra1502@gmail.com>
2026-03-15 10:38:27 +0530

    Database Connection

--- DAY-6/server.js
+++ DAY-6/server.js
@@ -3,10 +3,16 @@
 const app=require("./src/app");
 const mongoose=require("mongoose");
 
-// function connectToDb(){
+function connectToDb(){
+    mongoose.connect("mongodb+srv://shreymishra1502_db_user:bB796RupyBQD8htG@cluster0.dbja7dv.mongodb.net/day-6")
+    .then(()=>{
+        console.log("Connected to Database");
 
-// }
+    });
 
+}
+
+connectToDb();
 
 app.listen("3000",()=>{
     console.log("server is running on port 3000");
