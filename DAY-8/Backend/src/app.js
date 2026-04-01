8467d3cdc9fa92a42135b5043d614c00562487a8
Shrey Mishra <shreymishra1502@gmail.com>
2026-04-01 08:57:49 +0530

    Fullstack deployment

--- DAY-8/Backend/src/app.js
+++ DAY-8/Backend/src/app.js
@@ -1,4 +1,4 @@
-// server ko create krna
+ // server ko create krna
 
 const express=require("express");
 const noteModel=require("./models/note.model");
@@ -9,6 +9,7 @@ const app=express();
 
 app.use(cors());// CORS policy ko implement krne ke liye ye middleware use krna padta hai, jisse hum apne frontend se backend ko access kar sakte hai, bina iske hum apne frontend se backend ko access nahi kar paenge, kyunki CORS policy ke according, ek domain ke resources ko dusre domain ke resources se access karne ki permission nahi hoti hai, isliye hume CORS policy ko implement krna padta hai, jisse hum apne frontend se backend ko access kar sakte hai. 
 app.use(express.json());// json data ko read krne ke liye ye middleware use krna padta hai, jisse hum request body me json data bhej sakte hai.
+app.use(express.static("./public"));// static files ko serve krne ke liye ye middleware use krna padta hai, jisse hum apne frontend ke static files ko serve kar sakte hai, jaise ki html, css, js files, images etc.
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello World");
@@ -62,6 +63,6 @@ app.use("*name",(req,res)=>{
     res.sendFile(path.join(__dirname,"../public/index.html"));
 })
 
-console.log(__dirname);
+//  
 
 module.exports=app; 
